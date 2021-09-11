bb8356e2765c20d5e429aefe664a308aabc85e40
Cody Beinert <cody@gamejobs.ninja>
2021-09-11 11:50:00 -0400

    attempt to calculate the distance between the player and the enemy (failed for now)

--- Assignment1-Unity/Assets/Scripts/Components/VectorToEnemy.cs
+++ Assignment1-Unity/Assets/Scripts/Components/VectorToEnemy.cs
@@ -4,13 +4,26 @@ using System.Collections;
 public class VectorToEnemy : MonoBehaviour
 {
 
+    public GameObject enemy;
+    Vector3 playerPos;
+    Vector3 enemyPos;
+
     /// <summary>
     /// Calculated vector from the player to enemy found by GameManager.GetEnemyObject
     /// </summary>
     /// <see cref="GameController.GetEnemyObject"/>
     /// <returns>The vector from the player to the enemy.</returns>
+    /// 
+
+    void Start() 
+    {
+        playerPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        enemyPos = new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z);
+    }
+
     public Vector3 GetVectorToEnemy()
     {
+
         return Vector3.zero;
     }
 
