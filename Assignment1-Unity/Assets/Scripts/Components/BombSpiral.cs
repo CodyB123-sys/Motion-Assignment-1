dc26620b78c8efde1ac562db5fad4d3c022f2c10
Cody Beinert <cody@gamejobs.ninja>
2021-09-18 16:45:59 -0400

    Update BombSpiral.cs

--- Assignment1-Unity/Assets/Scripts/Components/BombSpiral.cs
+++ Assignment1-Unity/Assets/Scripts/Components/BombSpiral.cs
@@ -32,7 +32,7 @@ public class BombSpiral : MonoBehaviour
         
         for (int i = 0; i < BombCount; i++)
         {
-            point = (new Vector3( Mathf.Cos(degrees * Mathf.Deg2Rad), Mathf.Sin(degrees * Mathf.Deg2Rad))  * radius) + playerPos;
+            point = (new Vector3(Mathf.Cos(degrees * Mathf.Deg2Rad), Mathf.Sin(degrees * Mathf.Deg2Rad))  * radius) + playerPos;
             
             radius += ((EndRadius-StartRadius)/BombCount);
             
