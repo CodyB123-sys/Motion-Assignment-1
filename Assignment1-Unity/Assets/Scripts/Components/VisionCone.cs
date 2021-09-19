68f1ec5fdea1e7ad80976eb78b8c94b82a13bf9f
Cody Beinert <cody@gamejobs.ninja>
2021-09-19 11:08:16 -0400

    begin attempting to implement the VisionCone and HomingMissile components

--- Assignment1-Unity/Assets/Scripts/Components/VisionCone.cs
+++ Assignment1-Unity/Assets/Scripts/Components/VisionCone.cs
@@ -6,6 +6,9 @@ public class VisionCone : MonoBehaviour
 
     public float AngleSweepInDegrees = 60;
     public float ViewDistance = 3;
+    Vector3 point1;
+    Vector3 point2;
+    Vector3 enemyPos;
 
     /// <summary>
     /// Calculates whether the player is inside the vision cone of an enemy as defined by the AngleSweepIndegrees
@@ -14,8 +17,18 @@ public class VisionCone : MonoBehaviour
     /// </summary>
     /// <see cref="GameController"/>
     /// <returns>Whether the player is within the enemy's vision cone.</returns>
+    /// 
+
     public bool IsPlayerInVisionCone()
     {
+        point1 = (transform.up - (new Vector3(Mathf.Cos((AngleSweepInDegrees * 1/2) * Mathf.Deg2Rad), Mathf.Sin((AngleSweepInDegrees * 1/2) * Mathf.Deg2Rad)) * ViewDistance)) + enemyPos;
+        point2 = (transform.up + (new Vector3(Mathf.Cos((AngleSweepInDegrees * 1/2) * Mathf.Deg2Rad), Mathf.Sin((AngleSweepInDegrees * 1/2) * Mathf.Deg2Rad)) * ViewDistance)) + enemyPos;
+
+        enemyPos = this.transform.position;
+
+        Debug.DrawLine(enemyPos, point1, Color.white, 100f);
+        Debug.DrawLine(enemyPos, point2, Color.white, 100f);
+
         return false;
     }
     
--- Assignment1-Unity/Assets/Scripts/Controllers/HomingMissile.cs
+++ Assignment1-Unity/Assets/Scripts/Controllers/HomingMissile.cs
@@ -5,11 +5,21 @@ public class HomingMissile : MonoBehaviour
 {
     public float ForwardSpeed = 1;
     public float RotateSpeedInDeg = 45;
+    Vector3 startingPos;
+
+    GameObject enemy;
+    GameObject player;
 
     // In Update, you should rotate and move the missile to rotate it towards the player.  It should move forward with ForwardSpeed and rotate at RotateSpeedInDeg.
     // Do not use the RotateTowards or LookAt methods.
     void Update()
     {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        player = GameObject.Find("Player");
 
+        startingPos = player.transform.up;
+
+        Instantiate(this, startingPos, Quaternion.identity);
+        
     }
 }
