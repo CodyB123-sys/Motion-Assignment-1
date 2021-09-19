b77406e47af09d5caad357adbca28ed46ec6452f
Cody Beinert <cody@gamejobs.ninja>
2021-09-19 13:30:13 -0400

    attempt the homing missile implementation further

--- Assignment1-Unity/Assets/Scripts/Controllers/HomingMissile.cs
+++ Assignment1-Unity/Assets/Scripts/Controllers/HomingMissile.cs
@@ -5,21 +5,38 @@ public class HomingMissile : MonoBehaviour
 {
     public float ForwardSpeed = 1;
     public float RotateSpeedInDeg = 45;
-    Vector3 startingPos;
-
-    GameObject enemy;
-    GameObject player;
+    
+    
 
     // In Update, you should rotate and move the missile to rotate it towards the player.  It should move forward with ForwardSpeed and rotate at RotateSpeedInDeg.
     // Do not use the RotateTowards or LookAt methods.
     void Update()
     {
-        enemy = GameObject.FindGameObjectWithTag("Enemy");
-        player = GameObject.Find("Player");
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        Vector3 enemyPos;
+        enemyPos = enemy.transform.position;
+        Vector3 targetDir = enemyPos - this.transform.position;
+        
+        
+        
+        
+        /*if (this.transform.position.x > enemyPos.x && this.transform.position.y > enemyPos.y)
+        {
+            targetDir.x = this.transform.position.x - enemyPos.x;
+            targetDir.y = this.transform.position.y - enemyPos.y;
+        }
+        else if (this.transform.position.x > enemyPos.x && this.transform.position.y > enemyPos.y)
+        { 
+            
+        }*/
 
-        startingPos = player.transform.up;
+            float angle = Vector3.Angle(targetDir, Vector3.up);
 
-        Instantiate(this, startingPos, Quaternion.identity);
+        
+       
+        transform.Translate(Vector3.up * ForwardSpeed * Time.deltaTime);
+        transform.Rotate(0, angle, 0);
+        
         
     }
 }
