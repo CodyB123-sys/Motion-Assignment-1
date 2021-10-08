ed83a0d05f7787eb660a4b88fd06df002d1fdce5
Cody Beinert <cody@gamejobs.ninja>
2021-10-08 19:23:39 -0400

    drive myself crazy more

--- Assignment1-Unity/Assets/Scripts/Components/VisionCone.cs
+++ Assignment1-Unity/Assets/Scripts/Components/VisionCone.cs
@@ -35,18 +35,19 @@ public class VisionCone : MonoBehaviour
 
         float enemyAngle = Mathf.Atan2(this.transform.up.y, this.transform.up.x) * Mathf.Rad2Deg;
         float enemyToPlayerAngle = Mathf.Atan2(-vectorToEnemyScript.GetVectorToEnemy().y, -vectorToEnemyScript.GetVectorToEnemy().x) * Mathf.Rad2Deg;
+        
         point1 = (new Vector3(Mathf.Cos((enemyAngle + (AngleSweepInDegrees/2)) * Mathf.Deg2Rad), Mathf.Sin((enemyAngle + (AngleSweepInDegrees/2)) * Mathf.Deg2Rad)) * ViewDistance) + enemyPos;
         point2 = (new Vector3(Mathf.Cos((enemyAngle - (AngleSweepInDegrees/2)) * Mathf.Deg2Rad), Mathf.Sin((enemyAngle - (AngleSweepInDegrees/2)) * Mathf.Deg2Rad)) * ViewDistance) + enemyPos;
 
 
         
 
-        Debug.Log("this is enemy to player angle " + enemyToPlayerAngle);
-        Debug.Log("This is the enemy angle " + enemyAngle);
-        Debug.Log(vectorToEnemyScript.GetDistanceToEnemy());
-        Debug.Log("this is point1 " + point1);
-        Debug.DrawLine(enemyPos, point1, Color.white, 100f);
-        Debug.DrawLine(enemyPos, point2, Color.white, 100f);
+        //Debug.Log("this is enemy to player angle " + enemyToPlayerAngle);
+        //Debug.Log("This is the enemy angle " + enemyAngle);
+        //Debug.Log(vectorToEnemyScript.GetDistanceToEnemy());
+        //Debug.Log("this is point1 " + point1);
+        //Debug.DrawLine(enemyPos, point1, Color.white, 100f);
+        //Debug.DrawLine(enemyPos, point2, Color.white, 100f);
 
         if (enemyToPlayerAngle >= (enemyAngle - (AngleSweepInDegrees/2)) && enemyToPlayerAngle <= (enemyAngle + (AngleSweepInDegrees/2)) && vectorToEnemyScript.GetDistanceToEnemy() <= ViewDistance)
         {
